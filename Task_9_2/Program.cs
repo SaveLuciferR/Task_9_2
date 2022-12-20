b9fb85425a8da343ea913a86b15419787c1204a2
SaveLuciferR <cmudrec@mail.ru>
2022-12-20 18:47:02 +0300

    Добавьте файлы проекта.

---
+++ Task_9_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_9_2
+{
+	class Program
+	{
+		static void Main()
+		{
+			StreamReader fileIn = new StreamReader("text.txt");
+			string text = fileIn.ReadToEnd();
+			fileIn.Close();
+
+			string afterText = "";
+
+			string[] newText = Regex.Split(text, "[\n]+");
+
+			foreach (string s in newText)
+			{
+				afterText += s[0] + " ";
+			}
+
+			Console.WriteLine(afterText);
+		}
+	}
+}
